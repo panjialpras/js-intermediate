516026aa5783fcf7d4a49d8a264ee82f676ed450
Panji Al Muqsith Prasetyo <panjialpras.work@gmail.com>
2024-05-09 14:38:57 +0700

    Object literal

--- Object.js
+++ Object.js
@@ -1,1 +1,25 @@
-console.log('info')
+// Object literal
+
+let Karyawan = {
+    "name": "Panji",
+    "age": 22,
+    "sallary": 23456,
+    responsibility: function (berapa) {
+        this.sallary = this.sallary + (berapa * 300000)
+        console.log(`Hi, ${this.name}, You gain Rp. ${this.sallary} of sallary`)
+    }
+}
+
+Karyawan.responsibility(6)
+
+let Karyawan1 = {
+  name: "Riko",
+  age: 23,
+  sallary: 12345,
+  responsibility: function (berapa) {
+    this.sallary = this.sallary + berapa * 300000;
+    console.log(`Hi, ${this.name}, You gain Rp. ${this.sallary} of sallary`);
+  },
+};
+
+Karyawan1.responsibility(6);
